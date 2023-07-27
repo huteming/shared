47570505c766308c3c97415e840aae033573afb6
Ando.Hu <ando.hu@zoom.us>
2023-07-27 10:45:52 +0800

    feat: 'no-unused-vars' 降级为 warn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 }
