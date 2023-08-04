f8829cee029238044bd2d11a63411ff3ccdc2973
Ando.Hu <ando.hu@zoom.us>
2023-08-04 15:20:20 +0800

    feat: 添加 jest 环境

---
+++ packages/helpers/tests/isNil.spec.ts
@@ -0,0 +1,16 @@
+import { isNil } from '..'
+
+describe('isNil', () => {
+  it('期望 null, undefined 返回 ture', () => {
+    expect(isNil(null)).toBe(true)
+    expect(isNil(undefined)).toBe(true)
+  })
+
+  it('期望 其他 返回 false', () => {
+    expect(isNil(0)).toBe(false)
+    expect(isNil('')).toBe(false)
+    expect(isNil(false)).toBe(false)
+    expect(isNil({})).toBe(false)
+    expect(isNil([])).toBe(false)
+  })
+})
