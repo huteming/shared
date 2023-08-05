8cbdd4bbf7dc047c32999c9be61082925ea9766a
Ando.Hu <ando.hu@zoom.us>
2023-08-05 14:56:35 +0800

    feat: 新增 useServerState

--- packages/helpers/src/utils.ts
+++ packages/helpers/src/utils.ts
@@ -1,5 +1,22 @@
 import { AnyFn, MaybeValueOrGetter } from './types'
 
+export function isDevelopment(): boolean {
+  return process.env.NODE_ENV === 'development'
+}
+
+export function isTest(): boolean {
+  return process.env.NODE_ENV === 'test'
+}
+
+export function logError(err: string | Error, info: string): void {
+  if (!isDevelopment()) {
+    return
+  }
+
+  console.warn(`Error in ${info}: "${err.toString()}"`)
+  console.error(err)
+}
+
 export function isNil(data: any): data is null | undefined {
   if (data === null || data === undefined) {
     return true
