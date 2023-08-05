323f6b494efe1beebfa739119030beb7c70529fe
Ando.Hu <ando.hu@zoom.us>
2023-08-05 14:46:46 +0800

    feat: 新增 react_18

---
+++ packages/react_18/src/routes/destroy.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { redirect } from 'react-router-dom'
+import { deleteContact } from '../contacts'
+
+export async function action({ params }: any) {
+  await deleteContact(params.contactId)
+  return redirect('/')
+}
