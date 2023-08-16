6762ca348f4a8ec63d20e42b8de0fcd3a27b8ed9
Ando.Hu <ando.hu@zoom.us>
2023-08-16 10:34:44 +0800

    chore: mock/* 中的文件全部改为 ts

--- packages/vue_2/jest.config.js
+++ packages/vue_2/jest.config.js
@@ -18,8 +18,8 @@ module.exports = {
     '^.+\\.vue$': '@vue/vue2-jest',
   },
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/mocks/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.ts',
     '^@/(.*)$': '<rootDir>/src/$1',
     '^tests/(.*)$': '<rootDir>/tests/$1',
   },
